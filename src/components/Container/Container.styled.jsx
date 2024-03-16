765faeaa896753746967491a2ef6883b2911aa34
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2024-03-16 16:31:00 +0200

    repository setup

---
+++ src/components/Container/Container.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #010101;
+    
+    margin-left: auto;
+    margin-right: auto;
+`
